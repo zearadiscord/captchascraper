2f57e7ef28a80eee426c2acbb5683fabfbf75267
zearakun <91083604+zearakun@users.noreply.github.com>
2022-07-13 20:05:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import re
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -9,20 +8,17 @@ with open("README.md", "r") as f:
 
 
 setup(name='captchascraper',
-      version="0.0.1",
+      version="0.0.8",
       description='Captcha Bypass API',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/zearakun/captchascraper',
       install_requires=['requests'],
       author='zearadiscord',
-      author_email='info@2captcha.com',
       packages=find_packages(),
-      include_package_data=True,
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.6',
-      test_suite='tests')
+      ]
+     )

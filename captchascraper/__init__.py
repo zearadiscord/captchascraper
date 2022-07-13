0df01794a6cc8d3b4cc89ccf0b818f755ad32a64
zearakun <91083604+zearakun@users.noreply.github.com>
2022-07-13 20:00:47 +0900

    Update __init__.py

--- captchascraper/__init__.py
+++ captchascraper/__init__.py
@@ -6,12 +6,12 @@ def get(url,apikey,**kwargs):
       return requests.get(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
       
 def post(url,apikey,data,**kwargs):
-      return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
+      return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,**kwargs)
       
 def put(url,apikey,**kwargs):
       return requests.put(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
 def patch(url,apikey,data,**kwargse):
-      return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
+      return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,**kwargs)
 
 def delete(url,apikey,**kwargs):
       return requests.delete(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)

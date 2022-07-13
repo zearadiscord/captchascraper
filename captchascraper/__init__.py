5bc66da92ba319c441a5df688488cbc0941d66ff
zearakun <91083604+zearakun@users.noreply.github.com>
2022-07-13 19:34:01 +0900

    Update __init__.py

--- captchascraper/__init__.py
+++ captchascraper/__init__.py
@@ -2,31 +2,31 @@ import requests
 
 
 
-def get(url,headers=None,cookie=None,proxies=None,apikey):
+def get(url,apikey,headers=None,cookie=None,proxies=None):
       if proxies:
         return requests.get(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,cookie=None,headers=headers,proxies=proxies)
       if not proxies:
         return requests.get(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,cookie=cookie,headers=headers)
       
-def post(url,data,headers=None,cookie=None,proxies=None,apikey):
+def post(url,apikey,data,headers=None,cookie=None,proxies=None):
       if proxies:
         return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,cookie=None,headers=headers,proxies=proxies)
       if not proxies:
         return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,cookie=cookie,headers=headers)
       
-def put(url,headers=None,cookie=None,proxies=None,apikey):
+def put(url,apikey,headers=None,cookie=None,proxies=None):
       if proxies:
         return requests.put(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,cookie=cookie,headers=headers,proxies=proxies)
       if not proxies:
         return requests.put(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,cookie=cookie,headers=headers)
 
-def patch(url,data,headers=None,cookie=None,proxies=None,apikey):
+def patch(url,apikey,data,headers=None,cookie=None,proxies=None):
       if proxies:
         return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,cookie=cookie,headers=headers,proxies=proxies)
       if not proxies:
         return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,cookie=cookie,headers=headers)
 
-def delete(url,headers=None,cookie=None,proxies=None,apikey):
+def delete(url,apikey,headers=None,cookie=None,proxies=None):
       if proxies:
         return requests.delete(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,cookie=cookie,headers=headers,proxies=proxies)
       if not proxies:

379c35542d89e1f8d8c720671cff25997123af4c
zearakun <91083604+zearakun@users.noreply.github.com>
2022-07-14 11:23:22 +0900

    Update __init__.py

--- captchascraper/__init__.py
+++ captchascraper/__init__.py
@@ -2,16 +2,16 @@ import requests
 
 
 
-def get(url,apikey,**kwargs):
-      return requests.get(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
+def get(url,apikey,headers=None,**kwargs):
+      return requests.get(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",**kwargs)
       
 def post(url,apikey,data,**kwargs):
-      return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,**kwargs)
+      return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",data=data,**kwargs)
       
 def put(url,apikey,**kwargs):
-      return requests.put(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
+      return requests.put(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",**kwargs)
 def patch(url,apikey,data,**kwargse):
-      return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,data=data,**kwargs)
+      return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",data=data,**kwargs)
 
 def delete(url,apikey,**kwargs):
-      return requests.delete(f"http://api.scraperapi.com?api_key={apikey}&url=" + url,**kwargs)
+      return requests.delete(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",**kwargs)
